a609b0ce0e5dc48e69c9d2238b87419af6f04ef8
Luis Guisado <lguisadom@gmail.com>
2021-06-09 18:15:13 -0500

    Uso de Cors para compartir recursos en API REST

--- src/main/java/com/lagm/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/lagm/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -3,6 +3,7 @@ package com.lagm.springboot.backend.apirest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.lagm.springboot.backend.apirest.models.entity.Cliente;
 import com.lagm.springboot.backend.apirest.models.services.IClienteService;
 
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
-	
+
 	@Autowired
 	/* @Autowired: Busca una implementación concreta y la inyecta */
 	private IClienteService clienteService;
-	
+
 	@GetMapping("/clientes")
 	public List<Cliente> index() {
 		return clienteService.findAll();
