217e2a441933ea82abf1b40044f8afe027bebc02
Luis Guisado <lguisadom@gmail.com>
2021-06-09 19:48:21 -0500

    Métodos para registrar y buscar en el RestController del API Rest

--- src/main/java/com/lagm/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/lagm/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -1,11 +1,17 @@
 package com.lagm.springboot.backend.apirest.controllers;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lagm.springboot.backend.apirest.models.entity.Cliente;
@@ -23,5 +29,19 @@ public class ClienteRestController {
 	@GetMapping("/clientes")
 	public List<Cliente> index() {
 		return clienteService.findAll();
+	}
+	
+	@GetMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public Cliente show(@PathVariable Long id) {
+		return clienteService.findById(id);
+	}
+	
+	@PostMapping("/clientes")
+	@ResponseStatus(HttpStatus.CREATED) // CREATED: 201, cuando se creó contenido 
+	// @ResponseStatus: Por defecto si no se asigna el ResponseStatus, el valor por defecto es HttpStatus.OK = 200
+	// @RequestBody: El objeto cliente viene en formato JSON dentro del cuerpo del request
+	public Cliente create(@RequestBody Cliente cliente) {
+		return clienteService.save(cliente);
 	}
 }
