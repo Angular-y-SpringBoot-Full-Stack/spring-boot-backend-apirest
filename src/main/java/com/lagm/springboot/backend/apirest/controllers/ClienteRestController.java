5734d2377801754053701939fb70cb1fbe12d5b4
Luis Guisado <lguisadom@gmail.com>
2021-06-23 11:30:45 -0500

    Implementando anotación @Valid en métodos handler create y update del controller

--- src/main/java/com/lagm/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/lagm/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -1,13 +1,19 @@
 package com.lagm.springboot.backend.apirest.controllers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -59,9 +65,44 @@ public class ClienteRestController {
 	@PostMapping("/clientes")
 	// @ResponseStatus: Por defecto si no se asigna el ResponseStatus, el valor por defecto es HttpStatus.OK = 200
 	// @RequestBody: El objeto cliente viene en formato JSON dentro del cuerpo del request
-	public ResponseEntity<?> create(@RequestBody Cliente cliente) {
+	public ResponseEntity<?> create(@Valid @RequestBody Cliente cliente, BindingResult result) {
+		/* Intercepto de Spring: Intercepta el objeto cliente y valida cada valor de cada atributo del requestBody que envía angular en un formato json
+		 * la cual se traduce a nuestra clase entity Cliente
+		 * 1.
+		 * @Valid: con esta notación se activa la validación. Si no colocamos esto, a pesar de que tengamos las reglas de validación configuradas en la clase
+		 * entity Cliente, no se va a validar.
+		 * 
+		 * 2.
+		 * Inyectar al método create el objeto que contiene todos los mensajes de error con el cual se puede saber si ocurrió algún error en la 
+		 * validación: Se pasa el objeto BindingResult
+		 * */
 		Cliente clienteNew = null;
 		Map<String, Object> response = new HashMap<>();
+		
+		if (result.hasErrors()) {
+			// Si hay errores de validación, se obtienen los mensajes y se almacenan en un listado
+			// Forma clásica de manejar los errores utilizando un for, arraylist. 
+			/*List<String> errors = new ArrayList<>();
+			for (FieldError error : result.getFieldErrors()) {
+				errors.add("El campo '" + error.getField() + "' " + error.getDefaultMessage());
+			}*/
+			
+			// Con java 8 podremos usar el api stream para hacer lo mismo pero con menos código
+			List<String> errors = result.getFieldErrors()
+					.stream()
+					/*.map(err -> {
+						return "El campo '" + err.getField() + "' " + err.getDefaultMessage();
+						})*/
+					.map(err -> "El campo '" + err.getField() + "' " + err.getDefaultMessage())
+					.collect(Collectors.toList());
+			
+			response.put("errors", errors);
+			
+			
+			// BAD_REQUEST: 400, código utilizado cuando falla una validación en una aplicación web con http
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+		}
+		
 		try {
 			clienteNew = clienteService.save(cliente);
 		} catch (DataAccessException e) {
@@ -76,10 +117,21 @@ public class ClienteRestController {
 	}
 	
 	@PutMapping("/clientes/{id}")
-	public ResponseEntity<?> update(@RequestBody Cliente cliente, @PathVariable Long id) {
+	public ResponseEntity<?> update(@Valid @RequestBody Cliente cliente, BindingResult result, @PathVariable Long id) {
+		// Es importante el orden. El BindingResult debe estar justo después de Cliente pero antes de @PathVariable
 		Cliente clienteActual = clienteService.findById(id);
 		Cliente clienteUpdated = null;
 		Map<String, Object> response = new HashMap<>();
+		
+		if (result.hasErrors()) {
+			List<String> errors = result.getFieldErrors()
+					.stream()
+					.map(err -> "El campo '" + err.getField() + "' " + err.getDefaultMessage())
+					.collect(Collectors.toList());
+			
+			response.put("errors", errors);
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+		}
 		
 		if (clienteActual == null) {
 			response.put("mensaje", "Error: no se pudo editar. El cliente ID: ".concat(id.toString().concat(" no existe en la base de datos!")));
